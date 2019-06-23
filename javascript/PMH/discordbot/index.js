81b6e9d0a8fb5f7d94647bc335e8df79f559f7ef
PMHStudio <pmhstudio.pmh@gmail.com>
2019-06-23 16:28:14 +0900

    Detail Viewer Added

--- javascript/PMH/discordbot/index.js
+++ javascript/PMH/discordbot/index.js
@@ -23,15 +23,32 @@ bwt.on('ready', () => {
 bwt.on('message', (msg) => {
   if (msg.author.id === bwt.user.id) return
   if (msg.channel.id === '587089174331523075' || !msg.guild) {
-    let query = encodeURI(msg.content)
-    superagent.get('http://pmh.dps0340.xyz:8080/check/' + query).then((res) => {
-      let emb = new discord.RichEmbed()
-        .setAuthor(msg.author.username, msg.author.displayAvatarURL)
-        .setColor(res.body.result ? 0xff0000: 0x00ff00)
-        .setTitle(msg.content + ', 욕설여부: ' + (res.body.result ? '예' : '아니오'))
-        .setDescription('만약, 이 봇이 틀렸다면 PMH Studio / PMH#7086 를 불러주세요, 아직 배우는 단계라 잘 모를꺼에요')
-      msg.channel.send(emb)
-    }).catch((err) => console.error(err))
+    if (nu.content.endsWith('#detail')) {
+      let query = encodeURI(msg.content)
+      superagent.get('http://pmh.dps0340.xyz:8080/check/' + query).then((res) => {
+        let emb = new discord.RichEmbed()
+          .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+          .setColor(res.body.result ? 0xff0000: 0x00ff00)
+          .setTitle(msg.content + ', 욕설여부: ' + (res.body.result ? '예' : '아니오'))
+          .addBlankField()
+          .addField('Request Query', res.body.query)
+          .addField('Pre-Process', res.body.preProcess)
+          .addField(!res.body.process['KR-P1'] ? 'EN-P1': 'KR-P1', !res.body.process['KR-P1'] ? res.body.process['EN-P1']: res.body.process['KR-P1'])
+          if (res.body.process['KR-P2'] || res.body.process['EN-P2']) emb.addField(!res.body.process['KR-P2'] ? 'EN-P2': 'KR-P2', !res.body.process['KR-P2'] ? res.body.process['EN-P2']: res.body.process['KR-P2'])
+          if (res.body.process['KR-P3'] || res.body.process['EN-P3']) emb.addField(!res.body.process['KR-P3'] ? 'EN-P3': 'KR-P3', !res.body.process['KR-P3'] ? res.body.process['EN-P3']: res.body.process['KR-P3'])
+        msg.channel.send(emb)
+      }).catch((err) => console.error(err))
+    } else {
+      let query = encodeURI(msg.content)
+      superagent.get('http://pmh.dps0340.xyz:8080/check/' + query).then((res) => {
+        let emb = new discord.RichEmbed()
+          .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+          .setColor(res.body.result ? 0xff0000: 0x00ff00)
+          .setTitle(msg.content + ', 욕설여부: ' + (res.body.result ? '예' : '아니오'))
+          .setDescription('만약, 이 봇이 틀렸다면 PMH Studio / PMH#7086 를 불러주세요, 아직 배우는 단계라 잘 모를꺼에요')
+        msg.channel.send(emb)
+      }).catch((err) => console.error(err))
+    }
   }
 })
 
@@ -39,14 +56,31 @@ bwt.on('messageUpdate', (old, nu) => {
   if (old.content === nu.content) return
   if (nu.author.id === bwt.user.id) return
   if (nu.channel.id === '587089174331523075' || !nu.guild) {
-    let query = encodeURI(nu.content)
-    superagent.get('http://pmh.dps0340.xyz:8080/check/' + query).then((res) => {
-      let emb = new discord.RichEmbed()
-        .setAuthor(nu.author.username, nu.author.displayAvatarURL)
-        .setColor(res.body.result ? 0xff0000: 0x00ff00)
-        .setTitle(nu.content + ', 욕설여부: ' + (res.body.result ? '예' : '아니오'))
-        .setDescription('만약, 이 봇이 틀렸다면 PMH Studio / PMH#7086 를 불러주세요, 아직 배우는 단계라 잘 모를꺼에요')
-      nu.channel.send(emb)
-    }).catch((err) => console.error(err))
+    if (nu.content.endsWith('#detail')) {
+      let query = encodeURI(nu.content)
+      superagent.get('http://pmh.dps0340.xyz:8080/check/' + query).then((res) => {
+        let emb = new discord.RichEmbed()
+          .setAuthor(nu.author.username, nu.author.displayAvatarURL)
+          .setColor(res.body.result ? 0xff0000: 0x00ff00)
+          .setTitle(nu.content + ', 욕설여부: ' + (res.body.result ? '예' : '아니오'))
+          .addBlankField()
+          .addField('Request Query', res.body.query)
+          .addField('Pre-Process', res.body.preProcess)
+          .addField(!res.body.process['KR-P1'] ? 'EN-P1': 'KR-P1', !res.body.process['KR-P1'] ? res.body.process['EN-P1']: res.body.process['KR-P1'])
+          if (res.body.process['KR-P2'] || res.body.process['EN-P2']) emb.addField(!res.body.process['KR-P2'] ? 'EN-P2': 'KR-P2', !res.body.process['KR-P2'] ? res.body.process['EN-P2']: res.body.process['KR-P2'])
+          if (res.body.process['KR-P3'] || res.body.process['EN-P3']) emb.addField(!res.body.process['KR-P3'] ? 'EN-P3': 'KR-P3', !res.body.process['KR-P3'] ? res.body.process['EN-P3']: res.body.process['KR-P3'])
+        nu.channel.send(emb)
+      }).catch((err) => console.error(err))
+    } else {
+      let query = encodeURI(nu.content)
+      superagent.get('http://pmh.dps0340.xyz:8080/check/' + query).then((res) => {
+        let emb = new discord.RichEmbed()
+          .setAuthor(nu.author.username, nu.author.displayAvatarURL)
+          .setColor(res.body.result ? 0xff0000: 0x00ff00)
+          .setTitle(nu.content + ', 욕설여부: ' + (res.body.result ? '예' : '아니오'))
+          .setDescription('만약, 이 봇이 틀렸다면 PMH Studio / PMH#7086 를 불러주세요, 아직 배우는 단계라 잘 모를꺼에요')
+        nu.channel.send(emb)
+      }).catch((err) => console.error(err))
+    }
   }
 })
